24e9abbfb2c53cf1aea1530f006542a19117785f
Sarah Williams <s.williams@qcif.edu.au>
2022-07-13 15:47:28 +1000

    installation scripts

---
+++ scripts/lib_intstallation.R
@@ -0,0 +1,66 @@
+
+# First an up to date version of R (the most recent will do) - 4.2+
+
+
+# Cran
+install.packages("tidyverse")
+install.packages("Seurat")
+install.packages("patchwork")
+
+
+# github
+install.packages("remotes")
+install.packages("devtools")
+remotes::install_github('satijalab/seurat-wrappers')
+devtools::install_github('satijalab/seurat-data')
+
+devtools::install_github("immunogenomics/harmony")
+devtools::install_github("powellgenomicslab/scPred")
+
+# And bioconductor (again, the most recent) - v 3.9 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("SingleCellExperiment")
+BiocManager::install("HDF5Array")
+BiocManager::install("SingleR")
+BiocManager::install("celldex")   #download????
+BiocManager::install("limma")
+BiocManager::install("edgeR")
+BiocManager::install("CHETAH") 
+
+
+
+#---------------------------
+# Some loading of datasets into cache. 
+# Less important.
+# Run these R commands.
+library(celldex)
+celldex::HumanPrimaryCellAtlasData()
+
+library(SeuratData)
+data("hcabm40k")  # COuld be replaced with a preset object
+
+
+
+
+
+#---------------------------
+# And monocle.
+# We probably will *not* use monocle this time. 
+# However, if its easy, it would be nice to have it there (maybe reuse the image for a future workshop)
+# If its trivial, could it be installed please? 
+# If it isn't trivial, then don't bother.
+if (FALSE) {
+  # Monocle has a depenancy on the gdal tool (via package sf), which needs to be installed before the library.
+  # See:   https://cole-trapnell-lab.github.io/monocle3/docs/installation/
+  
+  # Install gdal
+  # apt-get install libgdal-dev
+  #
+  # 
+  # Then
+  devtools::install_github('cole-trapnell-lab/monocle3')
+}
+
+
