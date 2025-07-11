440d11cbacf308851a98116f09e5a781762f7fc0
Sarah Williams <s.williams@qcif.edu.au>
2025-07-11 14:56:44 +1000

    slides

--- scripts/required_installs.R
+++ scripts/required_installs.R
@@ -1,3 +1,5 @@
+# For windows only:
+install.packages("RTools")
 
 # Packages from Cran
 install.packages("tidyverse")
@@ -5,18 +7,23 @@ install.packages("Seurat")
 install.packages("patchwork")
 install.packages("hdf5r") #not really needed, only used to prepare 10Xpbmc.
 install.packages("clustree")
-install.packages("harmony") #formerly dev version- devtools::install_github("immunogenomics/harmony")
+#install.packages("harmony") #formerly dev version- devtools::install_github("immunogenomics/harmony"). Only used for harmony section, not in workshop
+
 
 # packages from github
+install.packages("remotes")
+# If seurat-wrappers gives errors about Matrix package,
+# Try to install this specific version, and retry:
+#  remotes::install_version("Matrix", version = "1.6-5")
 remotes::install_github('satijalab/seurat-wrappers')
-devtools::install_github('satijalab/seurat-data')
+remotes::install_github('satijalab/seurat-data')
+remotes::install_github('immunogenomics/presto') #optional, speeds up clustering
+
 
 
 # Bioconductor packages
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
-
 BiocManager::install("SingleCellExperiment")
 BiocManager::install("HDF5Array")
 BiocManager::install("SingleR")
